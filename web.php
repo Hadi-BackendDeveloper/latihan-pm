6c57374f99e5e917582cf17dba0c7d9217ef8d7f
Nurhadi <161283296+Hadi-BackendDeveloper@users.noreply.github.com>
2025-02-17 21:58:43 +0700

    Delete web.php

--- web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [UserController::class, 'index']);
-Route::post('/login', [UserController::class, 'login'])->name('login.submit');
-Route::get('/pendaftaran', [UserController::class, 'pendaftaran']);
-Route::post('/register', [UserController::class, 'register'])->name('register');
